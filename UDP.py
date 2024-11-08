a3a42f64bfb7d1e93f5d5b6f727373aebe501aca
BengIslam7 <devmlpentBMI66@gmail.com>
2024-11-08 14:14:36 +0100

    Adding two Rasp Pi code that sends UDP packet to STM32

---
+++ UDP.py
@@ -0,0 +1,25 @@
+import socket
+import os
+import time
+os.system("sudo ifconfig eth0 up")
+time.sleep(5)
+UDP_IP = "10.10.0.111"  # Replace with the target IP address
+UDP_PORT = 7       # Replace with the target port
+
+message = "o"  # send the message via socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+try:
+    print("**Opening**")
+    sock.sendto(message.encode(), (UDP_IP, UDP_PORT))
+    print("UDP Packet sent to STM32F7")
+    print(message.encode())
+    print("*************")
+finally:
+    sock.close()
+    os.system("sudo ifconfig eth0 down")
+    time.sleep(5)
+
+
+
